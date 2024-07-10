5e0692d853f97cc8aeae757fe8b44d883b057a5a
Lucas Gabas <lgabas95@gmail.com>
2024-07-10 11:07:42 -0300

    Se sube correcion de nombre de la branch

--- calculator.js
+++ calculator.js
@@ -1,18 +1,21 @@
-// Crear un archivo .js que se encargue de solucionar operaciones matematicas basicas
-// (suma, resta, multiplicacion, division, raiz cuadrada y elevar a potencia n)
-// Cada operación debe ser una función (ejemplo function sum(a,b) { blah blah})
-// Si se hace una operacion que no se puede (dividir por cero) deben poder atrapar ese error y
-// mostrarlo en consola (recuerden no parar la ejecución de su archivo js capturar el error pero puede continuar operando)
+/* Crear un archivo .js que se encargue de solucionar operaciones matematicas basicas
+(suma, resta, multiplicacion, division, raiz cuadrada y elevar a potencia n)
+Cada operación debe ser una función (ejemplo function sum(a,b) { blah blah})
+Si se hace una operacion que no se puede (dividir por cero) deben poder atrapar ese error y
+mostrarlo en consola (recuerden no parar la ejecución de su archivo js capturar el error pero puede continuar operando)*/
 
 function calculator(){
     function add(a, b){
-        if (typeof a !== 'number' || typeof b !== 'number' ){
-            throw new Error('Error: all parameters must be numbers.')
-        }
-        else{
-            return a + b;
-        }
+        try{
+            if (typeof a !== 'number' || typeof b !== 'number' ){
+                throw new Error('Error: all parameters must be numbers.')
+            } else {
+                return a + b;
+            }
+    } catch(error){
+        console.log(error)
     }
+}
     function subtract(a, b){
         if (typeof a !== 'number' || typeof b !== 'number' ){
             throw new Error('Error: all parameters must be numbers.');
@@ -46,5 +49,6 @@ function calculator(){
 
 
 const myCalculator = calculator();
-console.log(myCalculator.subtract(1, 10.5))
-console.log(myCalculator.division(1, 2))
+// console.log(myCalculator.add('l', 1))
+// console.log(myCalculator.subtract(1, 10.5))
+console.log(myCalculator.division(1, 0))
